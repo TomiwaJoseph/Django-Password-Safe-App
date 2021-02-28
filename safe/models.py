54db61cfc2cce3827685418306fc3db9afa79c70
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-02-28 22:40:43 +0100

    Added encrypt method

--- safe/models.py
+++ safe/models.py
@@ -11,3 +11,31 @@ class SavedPassword(models.Model):
 
     def __str__(self):
         return str(self.saver)+' - '+str(self.website)
+
+    def de_encrypt(self, message, key):
+        SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789:;<=>?@'
+        translated = ''
+
+        for symbol in message:
+            symbolIndex = SYMBOLS.find(symbol)
+            if symbolIndex == -1:
+                translated += symbol
+            else:
+                symbolIndex += key
+
+                if symbolIndex >= len(SYMBOLS):
+                    symbolIndex -= len(SYMBOLS)
+                elif symbolIndex < 0:
+                    symbolIndex += len(SYMBOLS)
+
+                translated += SYMBOLS[symbolIndex]
+        return translated
+
+    def save(self, *args, **kwargs):
+        decrypt_key = 13
+        message = str(self.password)
+
+        self.password = self.de_encrypt(message, decrypt_key)
+        super().save()
+
+# j@2YafCVBc10
